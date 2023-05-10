20bac072dfb5460f312ee5da2659d433e1c8b0be
hyunjun <hyunjuntyler@gmail.com>
2023-05-10 13:46:55 +0900

    Initial Commit

---
+++ MC2_Chagok/MC2_ChagokApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_ChagokApp.swift
+//  MC2_Chagok
+//
+//  Created by hyunjun kim on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct MC2_ChagokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
